485ec09d53e569f912930b1203c3f425305b6fde
test-user <test@test.com>
2020-07-26 20:57:11 +0300

    wip

--- packages/priority-queue/src/index.ts
+++ packages/priority-queue/src/index.ts
@@ -18,51 +18,78 @@ async function main() {
 
   const inProgressQueueName = `queue-in-progress`
   const pendingQueueName = `queue-pending`
-  const bullmqRedisConnection: IORedis.Redis = new IORedis({ host: 'localhost', port: 6379 })
-  const inProgressQueue = new Queue(inProgressQueueName, { connection: bullmqRedisConnection })
+  const redisConnection: IORedis.Redis = new IORedis({ host: 'localhost', port: 6379 })
+  redisConnection.defineCommand('incIfLessOrThrow', {
+    numberOfKeys: 1,
+    lua: `
+local max_open_betlips_concurrently = tonumber(ARGV[1]);
+local account_id = KEYS[1];
 
-  new Worker(
-    inProgressQueueName,
-    async job => {
-      try {
-        await processBetSlip(job)
-      } finally {
-        await bullmqRedisConnection.decr(job.data.account)
-        await bullmqRedisConnection.publish(betslipEndedTopic, job.data.account)
-      }
-    },
-    { connection: bullmqRedisConnection, concurrency: 30_000 },
-  )
+local open_betslips_now = tonumber(redis.call('GET', account_id));
 
-  new Worker(
-    pendingQueueName,
-    async function handleJob(job) {
-      console.log('new pending job: ', process.env.NAME, job.data)
-      const someBetslipEndedPromise = new Promise(res => betslipEndedConnection.once('message', res))
-      const mollyAccounts = ['stavalfi1', 'stavalfi2', 'stavalfi3']
-      // await connection.multi({ pipeline: false })
-      // const statusesAsStrings = await Promise.all(mollyAccounts.map(account => connection.get(account)))
-      // const statuses = statusesAsStrings.map(status => Number(status))
-      console.log({ mollyAccounts })
-      // console.log({ statusesAsStrings })
-      // console.log({ statuses })
-      // const freeAccountIndex = statuses.findIndex(status => status < 8)
-      const freeAccountIndex = chance().integer({ min: 0, max: mollyAccounts.length - 1 })
-      console.log({ freeAccountIndex })
-      if (freeAccountIndex > -1) {
-        // await connection.set(mollyAccounts[freeAccountIndex], statuses[freeAccountIndex] + 1)
-        // await connection.exec()
-        return inProgressQueue.add(job.name, job.data)
-      } else {
-        // await connection.exec()
-        if (freeAccountIndex === -1) {
-          await someBetslipEndedPromise
-          await handleJob(job)
-        }
-      }
-    },
-    { connection: bullmqRedisConnection, concurrency: 1 },
-  )
+if open_betslips_now == nil or open_betslips_now < max_open_betlips_concurrently then 
+  redis.call('INCR', account_id);
+else
+  error("account is full");
+end
+`,
+  })
+  await redisConnection['incIfLessOrThrow'](mollyAccount, MAX_BETSLIPS)
+
+  const mollyAccount = 'stavalfi2'
+  console.log(await redisConnection.get(mollyAccount))
+  // @ts-ignore
+  await redisConnection['incIfLessOrThrow'](mollyAccount, 1)
+  console.log(await redisConnection.get(mollyAccount))
+
+  return
+
+  // const inProgressQueue = new Queue(inProgressQueueName, { connection: redisConnection })
+
+  // new Worker(
+  //   inProgressQueueName,
+  //   async job => {
+  //     try {
+  //       await processBetSlip(job)
+  //     } finally {
+  //       await redisConnection.decr(job.data.account)
+  //       await redisConnection.publish(betslipEndedTopic, job.data.account)
+  //     }
+  //   },
+  //   { connection: redisConnection, concurrency: 30_000 },
+  // )
+
+  // new Worker(
+  //   pendingQueueName,
+  //   async function handleJob(job) {
+  //     console.log('new pending job: ', process.env.NAME, job.data)
+  //     const someBetslipEndedPromise = new Promise(res => betslipEndedConnection.once('message', res))
+  //     const mollyAccount = 'stavalfi1'
+  //     // @ts-ignore
+  //     await redisConnection['incIfLessOrThrow'](mollyAccount, 8)
+  //     // await connection.multi({ pipeline: false })
+  //     // const statusesAsStrings = await Promise.all(mollyAccounts.map(account => connection.get(account)))
+  //     // const statuses = statusesAsStrings.map(status => Number(status))
+  //     console.log({ mollyAccounts })
+  //     // console.log({ statusesAsStrings })
+  //     // console.log({ statuses })
+  //     // const freeAccountIndex = statuses.findIndex(status => status < 8)
+  //     const freeAccountIndex = chance().integer({ min: 0, max: mollyAccounts.length - 1 })
+  //     console.log({ freeAccountIndex })
+  //     if (freeAccountIndex > -1) {
+  //       // await connection.set(mollyAccounts[freeAccountIndex], statuses[freeAccountIndex] + 1)
+  //       // await connection.exec()
+  //       return inProgressQueue.add(job.name, job.data)
+  //     } else {
+  //       // await connection.exec()
+  //       if (freeAccountIndex === -1) {
+  //         await someBetslipEndedPromise
+  //         await handleJob(job)
+  //       }
+  //     }
+  //   },
+  //   { connection: redisConnection, concurrency: 1 },
+  // )
 }
 
 // eslint-disable-next-line no-floating-promise/no-floating-promise
--- packages/priority-queue/src/insert.ts
+++ packages/priority-queue/src/insert.ts
@@ -1,14 +1,32 @@
 /* eslint-disable no-console */
-import { Queue } from 'bullmq'
+import { Queue, Worker } from 'bullmq'
 import IORedis from 'ioredis'
 import chance from 'chance'
+import { setTimeout } from 'timers'
+
+async function subscribe(connection: IORedis.Redis, queueName: string, workerId: string) {
+  const worker = new Worker(
+    queueName,
+    async job => {
+      console.log(workerId, 'start', job.data)
+      await new Promise(res => setTimeout(res, 100))
+      console.log(workerId, 'end', job.data)
+    },
+    { connection: connection, concurrency: 30_000 },
+  )
+  await worker.waitUntilReady()
+}
 
 async function main() {
   const pendingQueueName = `queue-pending`
   const connection: IORedis.Redis = new IORedis({ host: 'localhost', port: 6379 })
   const pendingQueue = new Queue(pendingQueueName, { connection })
 
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 5; i++) {
+    await subscribe(connection, pendingQueueName, `worker-${i}`)
+  }
+
+  for (let i = 0; i < 100; i++) {
     await pendingQueue.add('lala', chance().hash())
   }
 }
