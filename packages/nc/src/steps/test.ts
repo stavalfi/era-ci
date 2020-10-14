8b7f2a35909f25eca5b74109c6d1c6af686a7b89
stavalfi <stavalfi@gmail.com>
2020-10-14 21:59:12 +0300

    wip

--- packages/nc/src/steps/test.ts
+++ packages/nc/src/steps/test.ts
@@ -1,17 +1,19 @@
 import { skipIfArtifactPackageJsonMissingScriptConstrain } from '../artifact-step-constrains'
-import { createStep, RunStrategy } from '../create-step'
+import { createStep, RunStrategy, UserRunStepOptions } from '../create-step'
 import { ExecutionStatus, Status } from '../types'
 import { execaCommand } from '../utils'
 import {
   skipIfArtifactStepResultMissingOrFailedInCacheConstrain,
   skipIfArtifactStepResultMissingOrPassedInCacheConstrain,
 } from '../artifact-step-constrains'
+import _ from 'lodash'
 
-export const test = createStep<{ testScriptName: string } | void, { testScriptName: string }>({
+export const test = createStep<{
+  testScriptName: string
+  beforeAll?: (options: Omit<UserRunStepOptions<void>, 'stepConfigurations'>) => Promise<void | unknown>
+  afterAll?: (options: Omit<UserRunStepOptions<void>, 'stepConfigurations'>) => Promise<void | unknown>
+}>({
   stepName: 'test',
-  normalizeStepConfigurations: async stepConfig => ({
-    testScriptName: (typeof stepConfig === 'object' && stepConfig.testScriptName) || 'test',
-  }),
   constrains: {
     onArtifact: [
       skipIfArtifactPackageJsonMissingScriptConstrain({
@@ -29,6 +31,11 @@ export const test = createStep<{ testScriptName: string } | void, { testScriptNa
   },
   run: {
     runStrategy: RunStrategy.perArtifact,
+    beforeAll: async options => {
+      if (options.stepConfigurations.beforeAll) {
+        await options.stepConfigurations.beforeAll(_.omit(options, 'stepConfigurations'))
+      }
+    },
     runStepOnArtifact: async ({ currentArtifact, stepConfigurations, log }) => {
       await execaCommand(`yarn run ${stepConfigurations.testScriptName}`, {
         cwd: currentArtifact.data.artifact.packagePath,
@@ -36,6 +43,11 @@ export const test = createStep<{ testScriptName: string } | void, { testScriptNa
         log,
       })
       return { errors: [], notes: [], executionStatus: ExecutionStatus.done, status: Status.passed }
+    },
+    afterAll: async options => {
+      if (options.stepConfigurations.afterAll) {
+        await options.stepConfigurations.afterAll(_.omit(options, 'stepConfigurations'))
+      }
     },
   },
 })
