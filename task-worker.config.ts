ed3c0326c51efcc3edff0fbbf61789cc97e9588e
stavalfi <stavalfi@gmail.com>
2021-01-02 19:39:39 +0200

    wip

---
+++ task-worker.config.ts
@@ -0,0 +1,19 @@
+import { config } from './packages/task-worker/dist/src/index'
+
+const {
+  GITHUB_RUN_NUMBER,
+  REDIS_ENDPOINT,
+  REDIS_PASSWORD,
+  // eslint-disable-next-line no-process-env
+} = process.env
+
+export default config({
+  queueName: `queue-${GITHUB_RUN_NUMBER}`,
+  waitBeforeExitMs: 10_000,
+  redis: {
+    url: REDIS_ENDPOINT!,
+    auth: {
+      password: REDIS_PASSWORD,
+    },
+  },
+})
