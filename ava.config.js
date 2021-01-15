53253ffcc8fb4b467567d770a24fe24a111bf94b
stavalfi <stavalfi@gmail.com>
2021-01-16 00:52:34 +0200

    compiling tests with @swc/register

--- ava.config.js
+++ ava.config.js
@@ -1,6 +1,6 @@
 export default {
   failWithoutAssertions: false,
   extensions: ['ts'],
-  require: ['ts-node/register'],
-  files: [`packages/**/*.spec.ts`, '*.spec.js'],
+  require: ['@swc/register'],
+  files: [`packages/**/*.spec.ts`],
 }
