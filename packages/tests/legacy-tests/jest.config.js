ad80916b14bc2cfe0c718a28c1d0364a563a778b
stavalfi <stavalfi@gmail.com>
2021-01-16 02:15:50 +0200

    wip

--- packages/tests/legacy-tests/jest.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const path = require('path')
-const jestConfig = require('../../../jest.config')
-
-module.exports = {
-  ...jestConfig,
-  testMatch: [path.join(__dirname, '__tests__/**/*.spec.ts')],
-}
