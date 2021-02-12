db2dda172a83aa33109c0532cf69d336add67db0
stavalfi <stavalfi@gmail.com>
2021-02-13 01:41:16 +0200

    split up long test files to multiple test files in legacy tests

--- packages/tests/legacy-tests/__tests__/minimal-publish/minimal-publish-1.spec.ts
+++ packages/tests/legacy-tests/__tests__/minimal-publish/minimal-publish-1.spec.ts
@@ -1,7 +1,7 @@
 import expect from 'expect'
-import { newEnv } from './prepare-test'
-import { manageStepResult } from './prepare-test/test-helpers'
-import { TargetType } from './prepare-test/types'
+import { newEnv } from '../prepare-test'
+import { manageStepResult } from '../prepare-test/test-helpers'
+import { TargetType } from '../prepare-test/types'
 
 const { createRepo } = newEnv()
 
@@ -113,103 +113,4 @@ describe('skip publish of package that did not change from the last publish', ()
     })
     expect(master2.ciProcessResult.failed).toBeFalsy()
   })
-})
-
-test('multiple packages - publish again changed package', async () => {
-  const { runCi, addRandomFileToPackage } = await createRepo({
-    packages: [
-      {
-        name: 'a',
-        version: '1.0.0',
-        targetType: TargetType.npm,
-      },
-      {
-        name: 'b',
-        version: '2.0.0',
-        targetType: TargetType.npm,
-      },
-    ],
-  })
-
-  const master1 = await runCi({
-    targetsInfo: {
-      npm: {
-        shouldPublish: true,
-        shouldDeploy: false,
-      },
-    },
-  })
-
-  expect(master1.published.get('a')?.npm?.versions).toEqual(['1.0.0'])
-  expect(master1.published.get('b')?.npm?.versions).toEqual(['2.0.0'])
-
-  await addRandomFileToPackage('a')
-
-  const master2 = await runCi({
-    targetsInfo: {
-      npm: {
-        shouldPublish: true,
-        shouldDeploy: false,
-      },
-    },
-  })
-
-  expect(master2.published.get('a')?.npm?.versions).toEqual(['1.0.0', '1.0.1'])
-  expect(master2.published.get('a')?.npm?.highestVersion).toEqual('1.0.1')
-  expect(master2.published.get('b')?.npm?.versions).toEqual(['2.0.0'])
-})
-
-test('no addtional publish of the same package with the exact same content', async () => {
-  const { runCi, modifyPackageJson } = await createRepo({
-    packages: [
-      {
-        name: 'a',
-        version: '1.0.0',
-        targetType: TargetType.npm,
-      },
-    ],
-  })
-
-  const master1 = await runCi({
-    targetsInfo: {
-      npm: {
-        shouldPublish: true,
-        shouldDeploy: false,
-      },
-    },
-  })
-  expect(master1.published.get('a')?.npm?.versions).toEqual(['1.0.0'])
-
-  await modifyPackageJson('a', packageJson => ({
-    ...packageJson,
-    author: 'stav',
-  }))
-
-  const master2 = await runCi({
-    targetsInfo: {
-      npm: {
-        shouldPublish: true,
-        shouldDeploy: false,
-      },
-    },
-  })
-  expect(master2.published.get('a')?.npm?.versions).toEqual(['1.0.0', '1.0.1'])
-  expect(master2.published.get('a')?.npm?.highestVersion).toEqual('1.0.1')
-
-  await modifyPackageJson('a', packageJson => {
-    const shallowCopy = { ...packageJson }
-    delete shallowCopy.author
-    return shallowCopy
-  })
-
-  const master3 = await runCi({
-    targetsInfo: {
-      npm: {
-        shouldPublish: true,
-        shouldDeploy: false,
-      },
-    },
-  })
-  expect(master3.published.get('a')?.npm?.versions).toEqual(['1.0.0', '1.0.1'])
-  expect(master3.published.get('a')?.npm?.highestVersion).toEqual('1.0.1')
 })
