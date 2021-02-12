ab229e1165b7c882eefc0a8318af997213f10359
stavalfi <stavalfi@gmail.com>
2021-02-13 01:43:39 +0200

    k8s step tests

---
+++ packages/tests/era-ci-tests/__tests__/steps/k8s-deployment/happy-flows.spec.ts
@@ -0,0 +1,356 @@
+import { createTest, isDeepSubset } from '@era-ci/e2e-tests-infra'
+import { dockerPublish, k8sDeployment } from '@era-ci/steps'
+import { createLinearStepsGraph } from '@era-ci/steps-graph'
+import { localSequentalTaskQueue } from '@era-ci/task-queues'
+import { execaCommand, ExecutionStatus, Status } from '@era-ci/utils'
+import expect from 'expect'
+import _ from 'lodash'
+
+const { createRepo, getResources, k8sHelpers } = createTest()
+
+test('redeploy to k8s and ensure the new image is running', async () => {
+  const { runCi, toActualName, testLog } = await createRepo(toActualName => ({
+    repo: {
+      packages: [
+        {
+          name: 'a',
+          version: '1.0.0',
+          src: {
+            'index.js': `
+            require('http')
+                .createServer((_req, res) => (res.writeHead(200), res.end('alive123')))
+                .listen(80)
+            `,
+          },
+          additionalFiles: {
+            Dockerfile: `\
+            FROM quay.io/eraci/node:15.7.0-alpine3.10
+            WORKDIR /usr/service
+            RUN apk add curl # we install it to ensure (at the end of the test) the correct image is running 
+            COPY packages/${toActualName('a')}/src/ src/
+            CMD node ./src/index.js
+            `,
+          },
+        },
+      ],
+    },
+    configurations: {
+      taskQueues: [localSequentalTaskQueue()],
+      steps: createLinearStepsGraph([
+        dockerPublish({
+          isStepEnabled: true,
+          dockerOrganizationName: getResources().quayNamespace,
+          dockerRegistry: getResources().dockerRegistry,
+          imageInstallArtifactsFromNpmRegistry: true,
+        }),
+        k8sDeployment({
+          isStepEnabled: true,
+          k8sNamesapce: 'default',
+          artifactNameToContainerName: ({ artifactName }) => toActualName(artifactName),
+          artifactNameToDeploymentName: ({ artifactName }) => toActualName(artifactName),
+          kubeConfigBase64: getResources().k8s.kubeConfigBase64,
+          failDeplomentOnPodError: true,
+        }),
+      ]),
+    },
+  }))
+
+  // this deployment will be redeployed to different image (from the dockerfile above)
+  await k8sHelpers.createK8sDeployment({
+    namespaceName: 'default',
+    containerName: toActualName('a'),
+    deploymentName: toActualName('a'),
+    fullImageName: 'nginx:1.18.0-alpine',
+    labels: { app: toActualName('a') },
+    podName: toActualName('a'),
+    portInContainer: 80,
+  })
+
+  const { passed } = await runCi()
+  expect(passed).toBeTruthy()
+
+  const podName = await k8sHelpers.findPodName(toActualName('a'))
+
+  const { stdout } = await execaCommand(`kubectl exec ${podName} -- curl localhost:80`, {
+    log: testLog,
+    stdio: 'pipe',
+  })
+  expect(stdout).toEqual('alive123')
+})
+
+test('era-ci try to redeploy a deployment which does not exist', async () => {
+  const { runCi, toActualName } = await createRepo(toActualName => ({
+    repo: {
+      packages: [
+        {
+          name: 'a',
+          version: '1.0.0',
+          src: {
+            'index.js': `
+            require('http')
+                .createServer((_req, res) => (res.writeHead(200), res.end('alive123')))
+                .listen(80)
+            `,
+          },
+          additionalFiles: {
+            Dockerfile: `\
+            FROM quay.io/eraci/node:15.7.0-alpine3.10
+            WORKDIR /usr/service
+            COPY packages/${toActualName('a')}/src/ src/
+            CMD node ./src/index.js
+            `,
+          },
+        },
+      ],
+    },
+    configurations: {
+      taskQueues: [localSequentalTaskQueue()],
+      steps: createLinearStepsGraph([
+        dockerPublish({
+          isStepEnabled: true,
+          dockerOrganizationName: getResources().quayNamespace,
+          dockerRegistry: getResources().dockerRegistry,
+          imageInstallArtifactsFromNpmRegistry: true,
+        }),
+        k8sDeployment({
+          isStepEnabled: true,
+          k8sNamesapce: 'default',
+          artifactNameToContainerName: ({ artifactName }) => toActualName(artifactName),
+          artifactNameToDeploymentName: ({ artifactName }) => toActualName(artifactName),
+          kubeConfigBase64: getResources().k8s.kubeConfigBase64,
+          failDeplomentOnPodError: true,
+        }),
+      ]),
+    },
+  }))
+
+  const { passed, flowLogs } = await runCi()
+  expect(passed).toBeFalsy()
+  expect(flowLogs).toEqual(expect.stringContaining(`deployments.apps \\"${toActualName('a')}\\" not found`))
+})
+
+test('failDeplomentOnPodError=true - redeploy to k8s fails because the new image throw exceptions - ensure the ci notify about a pod error', async () => {
+  const { runCi, toActualName } = await createRepo(toActualName => ({
+    repo: {
+      packages: [
+        {
+          name: 'a',
+          version: '1.0.0',
+          src: {
+            'index.js': 'throw new Error("error-123")',
+          },
+          additionalFiles: {
+            Dockerfile: `\
+            FROM quay.io/eraci/node:15.7.0-alpine3.10
+            WORKDIR /usr/service
+            RUN apk add curl # we install it to ensure (at the end of the test) the correct image is running 
+            COPY packages/${toActualName('a')}/src/ src/
+            CMD node ./src/index.js
+            `,
+          },
+        },
+      ],
+    },
+    configurations: {
+      taskQueues: [localSequentalTaskQueue()],
+      steps: createLinearStepsGraph([
+        dockerPublish({
+          isStepEnabled: true,
+          dockerOrganizationName: getResources().quayNamespace,
+          dockerRegistry: getResources().dockerRegistry,
+          imageInstallArtifactsFromNpmRegistry: true,
+        }),
+        k8sDeployment({
+          isStepEnabled: true,
+          k8sNamesapce: 'default',
+          artifactNameToContainerName: ({ artifactName }) => toActualName(artifactName),
+          artifactNameToDeploymentName: ({ artifactName }) => toActualName(artifactName),
+          kubeConfigBase64: getResources().k8s.kubeConfigBase64,
+          failDeplomentOnPodError: true,
+        }),
+      ]),
+    },
+  }))
+
+  // this deployment will be redeployed to different image (from the dockerfile above)
+  await k8sHelpers.createK8sDeployment({
+    namespaceName: 'default',
+    containerName: toActualName('a'),
+    deploymentName: toActualName('a'),
+    fullImageName: 'nginx:1.18.0-alpine',
+    labels: { app: toActualName('a') },
+    podName: toActualName('a'),
+    portInContainer: 80,
+  })
+
+  const { passed, flowLogs } = await runCi()
+  expect(passed).toBeFalsy()
+  expect(flowLogs).toEqual(
+    expect.stringMatching(
+      /failed to deploy. reason: pod: "(.*)" failed to run\. manually check the problem, commit a fix and run the CI again/,
+    ),
+  )
+})
+
+test('failDeplomentOnPodError=false, progressDeadlineSeconds=8 - redeploy to k8s fails because the new image throw exceptions -  ensure the ci notify about a deployment timeout', async () => {
+  const { runCi, toActualName } = await createRepo(toActualName => ({
+    repo: {
+      packages: [
+        {
+          name: 'a',
+          version: '1.0.0',
+          src: {
+            'index.js': 'throw new Error("error-123")',
+          },
+          additionalFiles: {
+            Dockerfile: `\
+            FROM quay.io/eraci/node:15.7.0-alpine3.10
+            WORKDIR /usr/service
+            RUN apk add curl # we install it to ensure (at the end of the test) the correct image is running 
+            COPY packages/${toActualName('a')}/src/ src/
+            CMD node ./src/index.js
+            `,
+          },
+        },
+      ],
+    },
+    configurations: {
+      taskQueues: [localSequentalTaskQueue()],
+      steps: createLinearStepsGraph([
+        dockerPublish({
+          isStepEnabled: true,
+          dockerOrganizationName: getResources().quayNamespace,
+          dockerRegistry: getResources().dockerRegistry,
+          imageInstallArtifactsFromNpmRegistry: true,
+        }),
+        k8sDeployment({
+          isStepEnabled: true,
+          k8sNamesapce: 'default',
+          artifactNameToContainerName: ({ artifactName }) => toActualName(artifactName),
+          artifactNameToDeploymentName: ({ artifactName }) => toActualName(artifactName),
+          kubeConfigBase64: getResources().k8s.kubeConfigBase64,
+          failDeplomentOnPodError: false,
+        }),
+      ]),
+    },
+  }))
+
+  // this deployment will be redeployed to different image (from the dockerfile above)
+  await k8sHelpers.createK8sDeployment({
+    namespaceName: 'default',
+    containerName: toActualName('a'),
+    deploymentName: toActualName('a'),
+    fullImageName: 'nginx:1.18.0-alpine',
+    labels: { app: toActualName('a') },
+    podName: toActualName('a'),
+    portInContainer: 80,
+    progressDeadlineSeconds: 8,
+  })
+
+  const { passed, jsonReport } = await runCi()
+  expect(passed).toBeFalsy()
+
+  const notes: string[] = _.get(jsonReport, [
+    'stepsResultOfArtifactsByStep',
+    1,
+    'data',
+    'artifactsResult',
+    0,
+    'data',
+    'artifactStepResult',
+    'notes',
+  ])
+
+  expect(notes).toEqual([
+    `failed to deploy. reason: the specified timeout (progressDeadlineSeconds) was reached: "8" seconds. manually check the problem, commit a fix and run the CI again`,
+  ])
+})
+
+test('running the flow again will cause to redeploy to k8s the same image again - ensure it does not hang the flow', async () => {
+  const { runCi, toActualName } = await createRepo(toActualName => ({
+    repo: {
+      packages: [
+        {
+          name: 'a',
+          version: '1.0.0',
+          src: {
+            'index.js': `
+            require('http')
+                .createServer((_req, res) => (res.writeHead(200), res.end('alive123')))
+                .listen(80)
+            `,
+          },
+          additionalFiles: {
+            Dockerfile: `\
+            FROM quay.io/eraci/node:15.7.0-alpine3.10
+            WORKDIR /usr/service
+            RUN apk add curl # we install it to ensure (at the end of the test) the correct image is running 
+            COPY packages/${toActualName('a')}/src/ src/
+            CMD node ./src/index.js
+            `,
+          },
+        },
+      ],
+    },
+    configurations: {
+      taskQueues: [localSequentalTaskQueue()],
+      steps: createLinearStepsGraph([
+        dockerPublish({
+          isStepEnabled: true,
+          dockerOrganizationName: getResources().quayNamespace,
+          dockerRegistry: getResources().dockerRegistry,
+          imageInstallArtifactsFromNpmRegistry: true,
+        }),
+        k8sDeployment({
+          isStepEnabled: true,
+          k8sNamesapce: 'default',
+          artifactNameToContainerName: ({ artifactName }) => toActualName(artifactName),
+          artifactNameToDeploymentName: ({ artifactName }) => toActualName(artifactName),
+          kubeConfigBase64: getResources().k8s.kubeConfigBase64,
+          failDeplomentOnPodError: true,
+        }),
+      ]),
+    },
+  }))
+
+  // this deployment will be redeployed to different image (from the dockerfile above)
+  await k8sHelpers.createK8sDeployment({
+    namespaceName: 'default',
+    containerName: toActualName('a'),
+    deploymentName: toActualName('a'),
+    fullImageName: 'nginx:1.18.0-alpine',
+    labels: { app: toActualName('a') },
+    podName: toActualName('a'),
+    portInContainer: 80,
+  })
+
+  await runCi()
+
+  const { passed, jsonReport } = await runCi()
+  expect(passed).toBeTruthy()
+
+  expect(
+    isDeepSubset(jsonReport, {
+      stepsResultOfArtifactsByStep: [
+        {
+          data: {
+            stepInfo: {
+              stepName: 'k8s-deployment',
+            },
+            artifactsResult: [
+              {
+                data: {
+                  artifactStepResult: {
+                    executionStatus: ExecutionStatus.aborted,
+                    status: Status.skippedAsPassed,
+                    notes: ['nothing new to deploy'],
+                  },
+                },
+              },
+            ],
+          },
+        },
+      ],
+    }),
+  ).toBeTruthy()
+})
---
+++ packages/tests/era-ci-tests/__tests__/steps/k8s-deployment/multiple-deployments-in-a-row.spec.ts
@@ -0,0 +1,181 @@
+import { createTest } from '@era-ci/e2e-tests-infra'
+import { dockerPublish, k8sDeployment } from '@era-ci/steps'
+import { createLinearStepsGraph } from '@era-ci/steps-graph'
+import { localSequentalTaskQueue } from '@era-ci/task-queues'
+import { execaCommand } from '@era-ci/utils'
+import expect from 'expect'
+import fs from 'fs'
+import _ from 'lodash'
+import path from 'path'
+
+const { createRepo, getResources, k8sHelpers } = createTest()
+
+test('2 deployments - all succeed', async () => {
+  const { runCi, toActualName, testLog, repoPath } = await createRepo(toActualName => ({
+    repo: {
+      packages: [
+        {
+          name: 'a',
+          version: '1.0.0',
+          src: {
+            'index.js': `
+            require('http')
+                .createServer((_req, res) => (res.writeHead(200), res.end('alive123')))
+                .listen(80)
+            `,
+          },
+          additionalFiles: {
+            Dockerfile: `\
+            FROM quay.io/eraci/node:15.7.0-alpine3.10
+            WORKDIR /usr/service
+            RUN apk add curl # we install it to ensure (at the end of the test) the correct image is running 
+            COPY packages/${toActualName('a')}/src/ src/
+            CMD node ./src/index.js
+            `,
+          },
+        },
+      ],
+    },
+    configurations: {
+      taskQueues: [localSequentalTaskQueue()],
+      steps: createLinearStepsGraph([
+        dockerPublish({
+          isStepEnabled: true,
+          dockerOrganizationName: getResources().quayNamespace,
+          dockerRegistry: getResources().dockerRegistry,
+          imageInstallArtifactsFromNpmRegistry: true,
+        }),
+        k8sDeployment({
+          isStepEnabled: true,
+          k8sNamesapce: 'default',
+          artifactNameToContainerName: ({ artifactName }) => toActualName(artifactName),
+          artifactNameToDeploymentName: ({ artifactName }) => toActualName(artifactName),
+          kubeConfigBase64: getResources().k8s.kubeConfigBase64,
+          failDeplomentOnPodError: true,
+        }),
+      ]),
+    },
+  }))
+
+  // this deployment will be redeployed to different image (from the dockerfile above)
+  await k8sHelpers.createK8sDeployment({
+    namespaceName: 'default',
+    containerName: toActualName('a'),
+    deploymentName: toActualName('a'),
+    fullImageName: 'nginx:1.18.0-alpine',
+    labels: { app: toActualName('a') },
+    podName: toActualName('a'),
+    portInContainer: 80,
+  })
+
+  await runCi()
+
+  await fs.promises.writeFile(
+    path.join(repoPath, 'packages', toActualName('a'), 'src', 'index.js'),
+    `require('http')
+        .createServer((_req, res) => (res.writeHead(200), res.end('alive1234')))
+        .listen(80)
+            `,
+    'utf-8',
+  )
+  await execaCommand(`git commit -am wip && git push`, { cwd: repoPath, stdio: 'pipe', shell: true, log: testLog })
+
+  const { passed } = await runCi()
+  expect(passed).toBeTruthy()
+
+  const podName = await k8sHelpers.findPodName(toActualName('a'))
+
+  const { stdout } = await execaCommand(`kubectl exec ${podName} -- curl localhost:80`, {
+    log: testLog,
+    stdio: 'pipe',
+  })
+  expect(stdout).toEqual('alive1234')
+})
+
+test('2 deployments - first succeed, second fail because of code-bug in the image', async () => {
+  const { runCi, toActualName, testLog, repoPath } = await createRepo(toActualName => ({
+    repo: {
+      packages: [
+        {
+          name: 'a',
+          version: '1.0.0',
+          src: {
+            'index.js': `
+            require('http')
+                .createServer((_req, res) => (res.writeHead(200), res.end('alive123')))
+                .listen(80)
+            `,
+          },
+          additionalFiles: {
+            Dockerfile: `\
+            FROM quay.io/eraci/node:15.7.0-alpine3.10
+            WORKDIR /usr/service
+            RUN apk add curl # we install it to ensure (at the end of the test) the correct image is running 
+            COPY packages/${toActualName('a')}/src/ src/
+            CMD node ./src/index.js
+            `,
+          },
+        },
+      ],
+    },
+    configurations: {
+      taskQueues: [localSequentalTaskQueue()],
+      steps: createLinearStepsGraph([
+        dockerPublish({
+          isStepEnabled: true,
+          dockerOrganizationName: getResources().quayNamespace,
+          dockerRegistry: getResources().dockerRegistry,
+          imageInstallArtifactsFromNpmRegistry: true,
+        }),
+        k8sDeployment({
+          isStepEnabled: true,
+          k8sNamesapce: 'default',
+          artifactNameToContainerName: ({ artifactName }) => toActualName(artifactName),
+          artifactNameToDeploymentName: ({ artifactName }) => toActualName(artifactName),
+          kubeConfigBase64: getResources().k8s.kubeConfigBase64,
+          failDeplomentOnPodError: true,
+        }),
+      ]),
+    },
+  }))
+
+  // this deployment will be redeployed to different image (from the dockerfile above)
+  await k8sHelpers.createK8sDeployment({
+    namespaceName: 'default',
+    containerName: toActualName('a'),
+    deploymentName: toActualName('a'),
+    fullImageName: 'nginx:1.18.0-alpine',
+    labels: { app: toActualName('a') },
+    podName: toActualName('a'),
+    portInContainer: 80,
+  })
+
+  await runCi()
+
+  await fs.promises.writeFile(
+    path.join(repoPath, 'packages', toActualName('a'), 'src', 'index.js'),
+    `throew new Error("123")`,
+    'utf-8',
+  )
+  await execaCommand(`git commit -am wip && git push`, { cwd: repoPath, stdio: 'pipe', shell: true, log: testLog })
+
+  const { passed, jsonReport } = await runCi()
+  expect(passed).toBeFalsy()
+
+  const notes: string[] = _.get(jsonReport, [
+    'stepsResultOfArtifactsByStep',
+    1,
+    'data',
+    'artifactsResult',
+    0,
+    'data',
+    'artifactStepResult',
+    'notes',
+  ])
+
+  expect(notes).toEqual([
+    expect.stringMatching(
+      /failed to deploy. reason: pod: "(.*)" failed to run\. manually check the problem, commit a fix and run the CI again/,
+    ),
+  ])
+})
