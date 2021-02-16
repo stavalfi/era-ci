c618e2dc3a1a56db007bbfce50c63dab279f4b3e
stavalfi <stavalfi@gmail.com>
2021-02-16 23:41:05 +0200

    ensure there are no uncommited changes after installing with yarn

--- packages/steps/src/install-root.ts
+++ packages/steps/src/install-root.ts
@@ -4,7 +4,7 @@ import {
 } from '@era-ci/constrains'
 import { createStep } from '@era-ci/core'
 import { LocalSequentalTaskQueue } from '@era-ci/task-queues'
-import { execaCommand } from '@era-ci/utils'
+import { execaCommand, ExecutionStatus, Status } from '@era-ci/utils'
 import fse from 'fs-extra'
 import path from 'path'
 
@@ -32,6 +32,26 @@ export const installRoot = createStep<LocalSequentalTaskQueue, { isStepEnabled: 
         stdio: 'inherit',
         log,
       })
+
+      log.info(`checking if there are uncommited changes after installing...`)
+
+      const isRepoClean = await execaCommand('git diff --exit-code', {
+        cwd: repoPath,
+        stdio: 'inherit',
+        log,
+        reject: false,
+      }).then(
+        () => true,
+        () => false,
+      )
+
+      if (!isRepoClean) {
+        return {
+          executionStatus: ExecutionStatus.done,
+          status: Status.failed,
+          notes: [`there are uncommited changes after installing dependencies. please commit and push them`],
+        }
+      }
     },
   }),
 })
