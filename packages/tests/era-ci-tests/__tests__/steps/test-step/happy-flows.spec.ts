e9297fcec8f1ef70313c0ba1e40a2a9a1befcc1c
stavalfi <stavalfi@gmail.com>
2021-02-27 21:31:22 +0200

    wip

--- packages/tests/era-ci-tests/__tests__/steps/test-step/happy-flows.spec.ts
+++ packages/tests/era-ci-tests/__tests__/steps/test-step/happy-flows.spec.ts
@@ -1,10 +1,10 @@
-import { createTest, isDeepSubset } from '@era-ci/e2e-tests-infra'
+import { createTest } from '@era-ci/e2e-tests-infra'
 import { test as testStep } from '@era-ci/steps'
 import { createLinearStepsGraph } from '@era-ci/steps-graph'
 import { taskWorkerTaskQueue } from '@era-ci/task-queues'
-import { ExecutionStatus, Status } from '@era-ci/utils'
-import { expect, test } from '@jest/globals'
+import { ExecutionStatus, Result, Status } from '@era-ci/utils'
 import chance from 'chance'
+import { DeepPartial } from 'ts-essentials'
 
 const { createRepo, getResources } = createTest()
 
@@ -42,28 +42,14 @@ test('run flow once - should pass without notes', async () => {
 
   const { jsonReport } = await runCi()
 
-  expect(
-    isDeepSubset(jsonReport, {
-      stepsResultOfArtifactsByArtifact: [
-        {
-          data: {
-            stepsResult: [
-              {
-                data: {
-                  artifactStepResult: {
-                    executionStatus: ExecutionStatus.done,
-                    status: Status.passed,
-                    notes: [],
-                    errors: [],
-                  },
-                },
-              },
-            ],
-          },
-        },
-      ],
-    }),
-  ).toBeTruthy()
+  expect(jsonReport.stepsResultOfArtifactsByArtifact[0].data.stepsResult[0].data.artifactStepResult).toMatchObject<
+    DeepPartial<Result>
+  >({
+    executionStatus: ExecutionStatus.done,
+    status: Status.passed,
+    notes: [],
+    errors: [],
+  })
 })
 
 test('run flow twice - second time should skip-as-passed with a note that indicates that the tests already passed in first flow', async () => {
@@ -102,30 +88,14 @@ test('run flow twice - second time should skip-as-passed with a note that indica
 
   const { jsonReport } = await runCi()
 
-  expect(
-    isDeepSubset(jsonReport, {
-      stepsResultOfArtifactsByArtifact: [
-        {
-          data: {
-            stepsResult: [
-              {
-                data: {
-                  artifactStepResult: {
-                    executionStatus: ExecutionStatus.aborted,
-                    status: Status.skippedAsPassed,
-                    notes: [
-                      `step: "${jsonReport.steps[0].data.stepInfo.displayName}" passed in flow: ${result1.flowId}`,
-                    ],
-                    errors: [],
-                  },
-                },
-              },
-            ],
-          },
-        },
-      ],
-    }),
-  ).toBeTruthy()
+  expect(jsonReport.stepsResultOfArtifactsByArtifact[0].data.stepsResult[0].data.artifactStepResult).toMatchObject<
+    DeepPartial<Result>
+  >({
+    executionStatus: ExecutionStatus.aborted,
+    status: Status.skippedAsPassed,
+    notes: [`step: "${jsonReport.steps[0].data.stepInfo.displayName}" passed in flow: ${result1.flowId}`],
+    errors: [],
+  })
 })
 
 test('run flow three times - third time should skip-as-passed with a note that indicates that the tests already passed in first flow', async () => {
@@ -166,28 +136,12 @@ test('run flow three times - third time should skip-as-passed with a note that i
 
   const { jsonReport } = await runCi()
 
-  expect(
-    isDeepSubset(jsonReport, {
-      stepsResultOfArtifactsByArtifact: [
-        {
-          data: {
-            stepsResult: [
-              {
-                data: {
-                  artifactStepResult: {
-                    executionStatus: ExecutionStatus.aborted,
-                    status: Status.skippedAsPassed,
-                    notes: [
-                      `step: "${jsonReport.steps[0].data.stepInfo.displayName}" passed in flow: ${result1.flowId}`,
-                    ],
-                    errors: [],
-                  },
-                },
-              },
-            ],
-          },
-        },
-      ],
-    }),
-  ).toBeTruthy()
+  expect(jsonReport.stepsResultOfArtifactsByArtifact[0].data.stepsResult[0].data.artifactStepResult).toMatchObject<
+    DeepPartial<Result>
+  >({
+    executionStatus: ExecutionStatus.aborted,
+    status: Status.skippedAsPassed,
+    notes: [`step: "${jsonReport.steps[0].data.stepInfo.displayName}" passed in flow: ${result1.flowId}`],
+    errors: [],
+  })
 })
