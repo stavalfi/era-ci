58c39f667cae24a5b3cbc8bae6c1d28cb24ec314
stavalfi <stavalfi@gmail.com>
2021-02-27 19:02:00 +0200

    remove eslint files from packages

--- packages/tests/legacy-tests/.eslintrc.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const eslint = require('../../../.eslintrc.js')
-
-module.exports = { ...eslint, rules: { ...eslint.rules, '@typescript-eslint/explicit-module-boundary-types': 'off' } }
